8f4049550d6cfb2aba43a6246ab95ff37157c670
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-09 20:57:10 +0700

    Update User.model.js

--- models/User.model.js
+++ models/User.model.js
@@ -2,37 +2,23 @@ const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
 
 const User = sequelize.define('User', {
-  discordId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    primaryKey: true,
-  },
-  username: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  avatar: {
-    type: DataTypes.STRING,
-  },
-  // --- CÁC TRƯỜNG MỚI CHO HỆ THỐNG KEY ---
-  activationKey: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  keyExpiresAt: {
-    type: DataTypes.DATE,
-    allowNull: true,
-  },
-  // Token tạm thời dùng trong quá trình lấy key mới
-  getkeyToken: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  },
-  // Key mới được tạo ra đang chờ người dùng nhập
-  pendingKey: {
-    type: DataTypes.STRING,
-    allowNull: true,
-  }
+    discordId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        primaryKey: true,
+    },
+    username: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    avatar: {
+        type: DataTypes.STRING,
+    },
 });
+
+// Import model Key sau khi User được định nghĩa để tránh lỗi cyclic dependency
+const Key = require('./Key.model');
+User.hasOne(Key, { foreignKey: 'userDiscordId' });
+Key.belongsTo(User, { foreignKey: 'userDiscordId' });
 
 module.exports = User;
