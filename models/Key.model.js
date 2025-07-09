9dce92488de21be34f9997ceb79fa9ea7b322e7d
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-09 21:06:49 +0700

    Update Key.model.js

--- models/Key.model.js
+++ models/Key.model.js
@@ -7,23 +7,29 @@ const Key = sequelize.define('Key', {
         autoIncrement: true,
         primaryKey: true,
     },
+    // Chuỗi key ngẫu nhiên mà người dùng sẽ nhập vào
     keyString: {
         type: DataTypes.STRING,
         allowNull: false,
         unique: true,
     },
+    // Thời điểm key sẽ hết hạn
     expiresAt: {
         type: DataTypes.DATE,
         allowNull: false,
     },
+    // ID của người dùng đang kích hoạt và sử dụng key này
     userDiscordId: {
         type: DataTypes.STRING,
-        allowNull: false,
+        allowNull: false, // Ban đầu khi tạo, nó được gán cho người tạo key. Sau đó user khác có thể kích hoạt
         references: {
             model: 'Users', // Tên bảng 'Users'
             key: 'discordId',
         }
     }
+}, {
+    // Tùy chọn thêm
+    timestamps: true, // Tự động thêm createdAt và updatedAt
 });
 
 module.exports = Key;
