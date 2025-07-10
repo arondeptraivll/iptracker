baf0556eb136e062e970e7e52088ab0e63fcf254
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-10 14:44:50 +0700

    Update Link.model.js

--- models/Link.model.js
+++ models/Link.model.js
@@ -15,12 +15,16 @@ const Link = sequelize.define('Link', {
         type: DataTypes.DATE,
         defaultValue: DataTypes.NOW,
     },
-    // ---- THAY ĐỔI CỘT CÀI ĐẶT ----
-    // Đổi tên từ requireCaptcha thành blockForeignIPs
     blockForeignIPs: {
         type: DataTypes.BOOLEAN,
         allowNull: false,
-        defaultValue: false, // Mặc định là TẮT (cho phép tất cả)
+        defaultValue: false,
+    },
+    // ---- CỘT CÀI ĐẶT MỚI ----
+    requestGPS: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false, // Mặc định là TẮT
     },
 });
 
