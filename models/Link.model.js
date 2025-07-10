e022823638e6d74a444c2721019797a9f29344a0
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-10 14:01:37 +0700

    Update Link.model.js

--- models/Link.model.js
+++ models/Link.model.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-// Các import khác không thay đổi
 
 const Link = sequelize.define('Link', {
     shortId: {
@@ -23,7 +22,5 @@ const Link = sequelize.define('Link', {
         defaultValue: false, // Mặc định là TẮT
     },
 });
-
-// Các định nghĩa quan hệ đã có trong models/index.js và không cần thay đổi
 
 module.exports = Link;
