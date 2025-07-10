5bef512467ede6166fb57441009907aab6a135ad
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-10 13:31:42 +0700

    Create index.js

---
+++ models/index.js
@@ -0,0 +1,31 @@
+const sequelize = require('../config/database');
+
+// 1. Import tất cả các model
+const User = require('./User.model');
+const Link = require('./Link.model');
+const Visit = require('./Visit.model');
+const Key = require('./Key.model');
+
+// 2. Thiết lập các mối quan hệ (Associations)
+
+// Mối quan hệ giữa User và Link
+User.hasMany(Link, { foreignKey: 'userDiscordId', onDelete: 'CASCADE' });
+Link.belongsTo(User, { foreignKey: 'userDiscordId' });
+
+// Mối quan hệ giữa Link và Visit
+Link.hasMany(Visit, { foreignKey: 'linkShortId', sourceKey: 'shortId', onDelete: 'CASCADE' });
+Visit.belongsTo(Link, { foreignKey: 'linkShortId', targetKey: 'shortId' });
+
+// Mối quan hệ giữa User và Key
+User.hasOne(Key, { foreignKey: 'userDiscordId', onDelete: 'CASCADE' });
+Key.belongsTo(User, { foreignKey: 'userDiscordId' });
+
+
+// 3. Export tất cả mọi thứ ra để sử dụng trong ứng dụng
+module.exports = {
+    sequelize, // Thể hiện kết nối DB
+    User,
+    Link,
+    Visit,
+    Key,
+};
