4c26a9be92e14b637d6855a5825941404422192e
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-10 14:00:38 +0700

    Update Link.model.js

--- models/Link.model.js
+++ models/Link.model.js
@@ -1,28 +1,29 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const User = require('./User.model');
+// Các import khác không thay đổi
 
 const Link = sequelize.define('Link', {
-  shortId: {
-    type: DataTypes.STRING,
-    allowNull: false,
-    unique: true,
-  },
-  targetUrl: {
-    type: DataTypes.STRING,
-    allowNull: false,
-  },
-  // THÊM TRƯỜNG MỚI
-  lastVisitedAt: {
-    type: DataTypes.DATE,
-    defaultValue: DataTypes.NOW, // Mặc định là thời điểm link được tạo
-  },
-}, {
-    // Thêm on cascade delete để khi xóa link, các Visit liên quan cũng bị xóa
-    onDelete: 'CASCADE',
+    shortId: {
+        type: DataTypes.STRING,
+        allowNull: false,
+        unique: true,
+    },
+    targetUrl: {
+        type: DataTypes.STRING,
+        allowNull: false,
+    },
+    lastVisitedAt: {
+        type: DataTypes.DATE,
+        defaultValue: DataTypes.NOW,
+    },
+    // ---- CỘT CÀI ĐẶT MỚI ----
+    requireCaptcha: {
+        type: DataTypes.BOOLEAN,
+        allowNull: false,
+        defaultValue: false, // Mặc định là TẮT
+    },
 });
 
-User.hasMany(Link, { foreignKey: 'userDiscordId' });
-Link.belongsTo(User, { foreignKey: 'userDiscordId' });
+// Các định nghĩa quan hệ đã có trong models/index.js và không cần thay đổi
 
 module.exports = Link;
