c09232b0cff682e2a6a2a5227483f7d5d963235f
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-10 13:31:12 +0700

    Update Key.model.js

--- models/Key.model.js
+++ models/Key.model.js
@@ -1,6 +1,5 @@
 const { DataTypes } = require('sequelize');
 const sequelize = require('../config/database');
-const User = require('./User.model');
 
 const Key = sequelize.define('Key', {
     id: {
@@ -17,27 +16,21 @@ const Key = sequelize.define('Key', {
         type: DataTypes.DATE,
         allowNull: false,
     },
-    // THAY ĐỔI QUAN TRỌNG:
-    // Key mới tạo sẽ chưa thuộc về ai cả, nên cột này phải cho phép NULL
     userDiscordId: {
         type: DataTypes.STRING,
-        allowNull: true, // Sửa từ 'false' thành 'true'
+        allowNull: true,
         references: {
             model: 'Users',
             key: 'discordId',
         }
     },
-    // Token tạm thời để định danh key chưa được nhận
     activationToken: {
         type: DataTypes.STRING,
-        allowNull: true, // Chỉ có giá trị khi key chưa được kích hoạt
+        allowNull: true,
         unique: true
     }
 });
 
-// Quan hệ này vẫn giữ nguyên vì một key CUỐI CÙNG sẽ thuộc về một User
-User.hasOne(Key, { foreignKey: 'userDiscordId', onDelete: 'CASCADE' });
-Key.belongsTo(User, { foreignKey: 'userDiscordId' });
-
+// KHÔNG CÒN ĐỊNH NGHĨA QUAN HỆ Ở ĐÂY NỮA
 
 module.exports = Key;
