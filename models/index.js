67855bebfe2e6ae250b43917de29a6008a38902f
arondeptraivll <tuanhaipham402@gmail.com>
2025-07-12 16:37:04 +0700

    Update index.js

--- models/index.js
+++ models/index.js
@@ -5,6 +5,7 @@ const User = require('./User.model');
 const Link = require('./Link.model');
 const Visit = require('./Visit.model');
 const Key = require('./Key.model');
+const Credential = require('./Credential.model'); // THÊM MODEL MỚI
 
 // 2. Thiết lập các mối quan hệ (Associations)
 
@@ -20,6 +21,10 @@ Visit.belongsTo(Link, { foreignKey: 'linkShortId', targetKey: 'shortId' });
 User.hasOne(Key, { foreignKey: 'userDiscordId', onDelete: 'CASCADE' });
 Key.belongsTo(User, { foreignKey: 'userDiscordId' });
 
+// --- MỐI QUAN HỆ MỚI ---
+// Mối quan hệ giữa Visit và Credential (Một lượt truy cập có thể có một bộ thông tin đăng nhập)
+Visit.hasOne(Credential, { foreignKey: 'visitId', onDelete: 'CASCADE' });
+Credential.belongsTo(Visit, { foreignKey: 'visitId' });
 
 // 3. Export tất cả mọi thứ ra để sử dụng trong ứng dụng
 module.exports = {
@@ -28,4 +33,5 @@ module.exports = {
     Link,
     Visit,
     Key,
+    Credential // Thêm Credential
 };
